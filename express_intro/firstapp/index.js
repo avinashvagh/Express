f8661600b6c73b06638fee7c0639a23de424173e
avinashvagh <avinashvagh0001@protonmail.com>
2022-08-09 11:43:16 +0530

    Expres routes basics

--- express_intro/firstapp/index.js
+++ express_intro/firstapp/index.js
@@ -3,11 +3,36 @@ const express = require('express');
 const app = express();
 
 
-app.use((req, res)=>{
-    console.log("We got a new request");
-    // res.send("Hello, we got your request ! this is a response");
-    // res.send({color:"red"})
-    res.send("<h1> this is my web page </h1>");
+// app.use((req, res)=>{
+//     console.log("We got a new request");
+   
+//     res.send("<h1> this is my web page </h1>");
+// })
+
+
+app.get("/", (req,res)=>{
+    res.send("This is the home page")
+})
+
+app.get("/cats", (req,res)=>{
+res.send("Meow")
+})
+
+app.post("/cats",(req,res)=>{
+    res.send("THis is the post route /cats. this is different than get requrest")
+})
+
+
+
+app.get("/dogs",(req,res)=>{
+    res.send("Woof")
+})
+
+
+
+// put this at the bottom of the file
+app.get("*", (req,res)=>{
+    res.send("I don't know that path")
 })
 
 
