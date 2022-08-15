0e0087de40d78721487e1048256939d9f5f74992
avinashvagh <avinashvagh0001@protonmail.com>
2022-08-15 21:48:54 +0530

    passing data to templates

--- express_templating/templating_demo/index.js
+++ express_templating/templating_demo/index.js
@@ -12,6 +12,10 @@ app.get("/", (req,res)=>{
     res.render("home.ejs")
 })
 
+app.get("/rand", (req,res)=>{
+    const num= Math.floor(Math.random()*10)+1
+    res.render("random.ejs", {rand: num})
+})
 app.listen(3000, ()=>{
     console.log("Listening on port 3000")
 })
