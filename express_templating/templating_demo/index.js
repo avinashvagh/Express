5d2f0c34b703ce027572780b33a814fa40255272
avinashvagh <avinashvagh0001@protonmail.com>
2022-08-18 20:40:27 +0530

    Loops in EJS

--- express_templating/templating_demo/index.js
+++ express_templating/templating_demo/index.js
@@ -12,6 +12,13 @@ app.get("/", (req,res)=>{
     res.render("home.ejs")
 })
 
+app.get("/cats", (req,res)=>{
+    const cats= [
+        "Blue", "Rocket", "Monty", "Stephanie","Winston"
+    ]
+    res.render("cats.ejs", {cats})
+})
+
 app.get("/r/:subreddit", (req,res)=>{
     const {subreddit} =req.params;
     res.render("subreddit.ejs", {subreddit})
