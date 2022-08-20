2bf0a221bfb7b3682c12429e01cb84168ceca96d
avinashvagh <avinashvagh0001@protonmail.com>
2022-08-20 14:32:28 +0530

    RESTful comment index

--- restful_routes/RestDemo/index.js
+++ restful_routes/RestDemo/index.js
@@ -2,11 +2,41 @@
 
 const express= require("express")
 const app=express()
+const path= require("path")
 
 //  you need to include this line 
 app.use(express.urlencoded({extended:true}))
 // if you use json to retrive data through json in postman than you need to include it 
 app.use(express.json())
+app.set("views",path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+
+const comments=[
+    {
+        username:"Todd",
+        comment: 'lol that is funny'
+    },
+    {
+        username:"skyler",
+        comment: 'I like to bird watching with my dog'
+    },
+    {
+        username:"LightYagami",
+        comment: 'plz delete your account'
+    },
+    {
+        username:"L",
+        comment: 'woof woof woof'
+    }
+
+]
+
+//  RESTful comment index 
+
+app.get("/comments", (req, res)=>{
+    res.render("comments/index", {comments})
+})
 
 
 
@@ -25,4 +55,8 @@ app.post("/tacos", (req,res)=>{
 app.listen(3000,()=>{
     console.log("ON PORT 3000!")
 })
+
+
+
+
 
