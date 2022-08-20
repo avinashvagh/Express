cd43bf3be4f8579f6aa7959ef9870ec2fdcac0bb
avinashvagh <avinashvagh0001@protonmail.com>
2022-08-21 00:47:03 +0530

    express method-override + restful comment+delete

--- restful_routes/RestDemo/index.js
+++ restful_routes/RestDemo/index.js
@@ -1,81 +1,120 @@
+const path = require('path');
+const methodOverride = require('method-override')
+const { v4: uuid } = require('uuid'); //For generating ID's
+const express = require('express');
+const app = express();
 
 
-const express= require("express")
-const app=express()
-const path= require("path")
-
-//  you need to include this line 
-app.use(express.urlencoded({extended:true}))
-// if you use json to retrive data through json in postman than you need to include it 
+//To parse form data in POST request body:
+app.use(express.urlencoded({ extended: true }))
+// To parse incoming JSON in POST request body:
 app.use(express.json())
-app.set("views",path.join(__dirname, "views"))
-app.set("view engine", "ejs")
+// To 'fake' put/patch/delete requests:
+app.use(methodOverride('_method'))
+// Views folder and EJS setup:
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-
-const comments=[
+// Our fake database:
+let comments = [
     {
-        id:1,
-        username:"Todd",
-        comment: 'lol that is funny'
+        id: uuid(),
+        username: 'Todd',
+        comment: 'lol that is so funny!'
     },
     {
-        id:2,
-        username:"skyler",
-        comment: 'I like to bird watching with my dog'
+        id: uuid(),
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
     },
     {
-        id:3,
-        username:"LightYagami",
-        comment: 'plz delete your account'
+        id: uuid(),
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
     },
     {
-        id:4,
-        username:"L",
+        id: uuid(),
+        username: 'onlysayswoof',
         comment: 'woof woof woof'
     }
-
 ]
+// **********************************
+// INDEX - renders multiple comments
+// **********************************
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+})
+// **********************************
+// NEW - renders a form
+// **********************************
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+// **********************************
+// CREATE - creates a new comment
+// **********************************
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id: uuid() })
+    res.redirect('/comments');
+})
+// *******************************************
+// SHOW - details about one particular comment
+// *******************************************
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment })
+})
+// *******************************************
+// EDIT - renders a form to edit a comment
+// *******************************************
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+})
+// *******************************************
+// UPDATE - updates a particular comment
+// *******************************************
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const foundComment = comments.find(c => c.id === id);
 
-//  RESTful comment index 
-
-app.get("/comments", (req, res)=>{
-    res.render("comments/index", {comments})
+    //get new text from req.body
+    const newCommentText = req.body.comment;
+    //update the comment with the data from req.body:
+    foundComment.comment = newCommentText;
+    //redirect back to index (or wherever you want)
+    res.redirect('/comments')
 })
 
-app.get("/comments/new", (req,res)=>{
-    res.render("comments/new")
+// *******************************************
+// DELETE/DESTROY- removes a single comment
+// *******************************************
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
 })
 
-// RESTFul commnets new 
-app.post("/comments", (req,res)=>{
-    const {username, comment}= req.body
-    comments.push({username, comment})
-    res.redirect("/comments")
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response")
 })
 
-app.get("/comments/:id", (req,res)=>{
-    const{id} = req.params;
-    const comment= comments.find(c=> c.id===parseInt(id))
-    res.render("comments/show", {comment})
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat} tacos`)
 })
 
-app.get("/tacos", (req, res)=>{
-    res.send("GET/tacos response")
-
-})
-
-app.post("/tacos", (req,res)=>{
-    // parsing the request body 
-    const {meat,qty} =req.body
-    res.send(`Ok, here are your ${qty} ${meat} tacos`)
-})
-
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("ON PORT 3000!")
 })
 
 
-
-
+// GET /comments - list all comments
+// POST /comments - Create a new comment 
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment
 
